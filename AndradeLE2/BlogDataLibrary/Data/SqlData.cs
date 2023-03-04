8b4f8d4a88befa6aefe4baf912de5d39b19fffdf
stu1413 <reuben321@yahoo.com>
2023-03-04 22:05:17 +0800

    adding LE2

---
+++ AndradeLE2/BlogDataLibrary/Data/SqlData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogDataLibrary.Data
+{
+    internal class SqlData
+    {
+        private ISqlDataAccess _db;
+        private const string connectionStringName = "SqlDB";
+
+        public SqlData(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+    }
+}
---
+++ AndradeLE2/BlogDataLibrary/Database/SqlDataAccess.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogDataLibrary.Database
+{
+    public class SqlDataAccess : ISqlDataAccess
+    {
+        private IConfiguration _config;
+
+        public SqlDataAccess(IConfiguration config)
+        { 
+            _config = config;
+        }
+
+        public List<T> LoadData<T, U>(string sqlSatement,
+                                                U parameters,
+                                                string connectionStringName,
+                                                bool isStoredProcedure)
+        {
+            CommandType commandType = CommandType.Text;
+            string connectionString =_config.GetConnectionString(connectionStringName);
+
+            if (isStoredProcedure)
+            {
+                commandType = CommandType.StoredProcedure;
+            }
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                List<T> rows = connection.Query<T>(sqlSatement, parameters,
+                    commandType: commandType).ToList();
+                return rows;
+            }
+        }
+
+        public void LoadData<T>(string sqlSatement,
+                                        T parameters,
+                                        string connectionStringName,
+                                        bool isStoredProcedure)
+        {
+            string connectionString = _config.GetConnectionString(connectionStringName);
+            CommandType commandType = CommandType.Text;
+
+            if (isStoredProcedure)
+            {
+                commandType = CommandType.StoredProcedure;
+            }
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Execute(sqlSatement, parameters, commandType: commandType);
+            }
+        }
+    }
+}
