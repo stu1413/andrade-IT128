f2d901608f5ff28ca83f8edda5771f98232bfb87
stu1413 <reuben321@yahoo.com>
2023-04-21 07:24:48 +0800

    adding LE5

---
+++ ANDRADELE4/frontend/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListPostsComponent } from './components/list-posts/list-posts.component';
+import { PostDetailComponent } from './components/post-detail/post-detail.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { RegisterPageComponent } from './components/register-page/register-page.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListPostsComponent,
+    PostDetailComponent,
+    LoginPageComponent,
+    RegisterPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ANDRADELE4/frontend/src/app/components/post-detail/post-detail.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Post } from 'src/app/models/post.model';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-post-detail',
+  templateUrl: './post-detail.component.html',
+  styleUrls: ['./post-detail.component.css']
+})
+export class PostDetailComponent {
+  private routeSub: Subscription = new Subscription();
+  private id: number = 0;
+
+  post?: Post;
+
+  constructor(
+    private route: ActivatedRoute,
+    private http: HttpClient
+  ) {  }
+
+  ngOnInit(): void {
+    this.routeSub = this.route.params.subscribe(params => {
+      this.id = params['id'];
+    })
+    this.initData();
+  }
+
+  initData(): void {
+    this.http.get<Post>("https://localhost:7259/api/post/" + this.id).subscribe({
+      next: (data: Post) => {
+        this.post = data;
+        console.log(this.post);
+      }
+    })
+  }
+
+}
